568c259216f11eed8501a58da721e7d16a5199f2
Savannah Ostrowski <savannahostrowski@gmail.com>
2023-07-03 14:28:39 -0700

    file output works

--- src/extension.ts
+++ src/extension.ts
@@ -2,11 +2,11 @@ import * as vscode from 'vscode';
 
 import * as ai from './ai';
 import path = require('path');
+import assert = require('assert');
 
 let apikey: string | undefined;
 
 export function activate(context: vscode.ExtensionContext) {
-
 	let disposable = vscode.commands.registerCommand('ghost-vscode.generate', async () => {
 
 		// Ask for API key in command palette and store as environment variable
@@ -18,19 +18,19 @@ export function activate(context: vscode.ExtensionContext) {
 
 			process.env.OPENAI_API_KEY = apikey;
 		}
-		// get all files in workspace, ignore .json files, node_modules and .md, .gitignore
+
+		// Get all files in workspace, ignore .json files, node_modules and .md, .gitignore
 		const workspaceFiles = await vscode.workspace.findFiles('**/*', '{.*,node_modules,*.json,*.md,*.gitignore,LICENSE*}');
 		const fileNames = workspaceFiles.map(file => file.fsPath.split('/').pop());
 
-		// Make GPT request
+		// Detect project language(s)
 		var confirmedLanguages = false;
-		var additionalProjectInfo: string | undefined;
 		var detectedLanguages: string | undefined;
 		var initialLangPrompt = true;
 
 		while (!confirmedLanguages && apikey) {
 			var prompt: string;
-			if (!additionalProjectInfo && !detectedLanguages && !initialLangPrompt) {
+			if (!detectedLanguages && !initialLangPrompt) {
 				prompt = `You said this project uses the following languages ${detectedLanguages} (detected from the following files: ${fileNames.join(', ')}). 
 				According to the user, this is not correct. Here's some additional info from the user: %v.
 				Return a comma-separated list of the languages used by this project.`;
@@ -39,7 +39,7 @@ export function activate(context: vscode.ExtensionContext) {
 				Return a comma-separated list with just the unique language names:${fileNames.join(', ')}. 
 				The response should not include any additional text other than the list of languages.`;
 				initialLangPrompt = false;
-			}			
+			}
 
 			var detectedLanguages = await ai.chatGPTRequest(prompt);
 
@@ -49,10 +49,8 @@ export function activate(context: vscode.ExtensionContext) {
 				return;
 			} else if (result === 'Yes') {
 				confirmedLanguages = true;
-				// Reset additionalProjectInfo for use in tasks
-				additionalProjectInfo = undefined;
 			} else {
-				additionalProjectInfo = await vscode.window.showInputBox({
+				detectedLanguages = await vscode.window.showInputBox({
 					placeHolder: "Enter languages (comma separated)",
 					prompt: "Give Ghost some more info about the languages used in this project",
 				});
@@ -61,42 +59,60 @@ export function activate(context: vscode.ExtensionContext) {
 
 		// Add tasks
 		var confirmedTasks = false;
+		var GHA: string | undefined;
 		var userDefinedTasks = await vscode.window.showInputBox({
 			placeHolder: "Enter tasks (comma separated)",
 			prompt: "What tasks should Ghost include in your GitHub Action workflow?",
 		});
-		
-		while(confirmedLanguages && userDefinedTasks && apikey) {
+
+		while (confirmedLanguages && userDefinedTasks && apikey && !confirmedTasks) {
 			var prompt = `For a ${detectedLanguages} program, generate a GitHub Action workflow that will include the following tasks: ${userDefinedTasks}. 
-				${additionalProjectInfo ? `Here's some additional info from the user: ${additionalProjectInfo}.` : ''}
 				Name it "Ghost-generated pipeline". Leave placeholders for things like version and at the end of generating the
-				GitHub Action, tell the user what their next steps should be in a comment`;
+				GitHub Action, tell the user what their next steps should be in a comment. Do not surround the content with a codeblock. Make sure it's
+				valid YAML.`;
 
-			var GHA = await ai.chatGPTRequest(prompt);
-			console.log(GHA);
+			GHA = await ai.chatGPTRequest(prompt);
 
-			// get current path
-			// if (GHA) {
-			// 	const currentPath = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
-			// 	const newFile = vscode.Uri.parse('untitled:' + path.join(currentPath, 'temp.yml'));
-			// 	vscode.workspace.openTextDocument(newFile).then(document => {
-			// 		const edit = new vscode.WorkspaceEdit();
-			// 		edit.insert(newFile, new vscode.Position(0, 0), GHA);
-			// 		return vscode.workspace.applyEdit(edit).then(success => {
-			// 			if (success) {
-			// 				vscode.window.showTextDocument(document);
-			// 			} else {
-			// 				vscode.window.showInformationMessage('Error!');
-			// 			}
-			// 		});
-			// 	});
-			// }
-			
+			const currentPath = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
+			assert(currentPath !== undefined);
+			const newFile = vscode.Uri.parse('untitled:' + path.join(currentPath, 'temp.yml'));
+
+			vscode.workspace.openTextDocument(newFile).then(document => {
+				const edit = new vscode.WorkspaceEdit();
+				assert(GHA !== undefined);
+				edit.insert(newFile, new vscode.Position(0, 0), GHA);
+				return vscode.workspace.applyEdit(edit).then(success => {
+					if (success) {
+						vscode.window.showTextDocument(document);
+					} else {
+						vscode.window.showInformationMessage('Error!');
+					}
+				});
+			});
+
+			// Confirm tasks
+			const result = await vscode.window.showInformationMessage(`Would you like to save the workflow?`, 'Yes', 'No, update tasks', 'Cancel');
+			if (result === "Cancel") {
+				return;
+			} else if (result === 'Yes') {
+				confirmedTasks = true;
+				
+				//Output GHA to a file called ghost-<timestamp>.yml in the .github/workflows directory
+				const currentPath = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
+				if (currentPath) {
+					const ghostDir = path.join(currentPath, '.github/workflows');
+					const ghostFile = path.join(ghostDir, `ghost-${Date.now()}.yml`);
+					vscode.workspace.fs.createDirectory(vscode.Uri.file(ghostDir));
+					vscode.workspace.fs.writeFile(vscode.Uri.file(ghostFile), Buffer.from(GHA ?? ''));
+				}
+			} else {
+				userDefinedTasks = await vscode.window.showInputBox({
+					placeHolder: "Enter tasks (comma separated)",
+					prompt: "What tasks should Ghost include in your GitHub Action workflow?",
+				});
+			}
 		}
-
-		//Confirm GHA
-
-		//Output to file
+		
 	});
 
 	context.subscriptions.push(disposable);
